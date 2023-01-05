65ed3ca7ddbe2f8dfe3c1a004ee8d8f8d8543345
Mochis <juan.mochiesp@gmail.com>
2023-01-05 23:31:30 +0100

    Day 4 haskell

---
+++ day04/haskell/d4p1p2.hs
@@ -0,0 +1,31 @@
+import Data.List.Split (splitOn)
+
+type Range = (Int, Int)
+
+parseRange :: String -> (Int, Int)
+parseRange = tuplify2 <$> map read . splitOn "-"
+
+parseRanges :: String -> (Range, Range)
+parseRanges = tuplify2 <$> map parseRange . splitOn ","
+
+tuplify2 :: [a] -> (a, a)
+tuplify2 [x, y] = (x, y)
+tuplify2 _ = error "Invalid list"
+
+fullyOverlaps :: Range -> Range -> Bool
+fullyOverlaps (a, b) (c, d) = (a <= c && b >= d) || (a >= c && b <= d)
+
+overlaps :: Range -> Range -> Bool
+overlaps (a, b) (c, d) = a <= d && b >= c
+
+main :: IO ()
+main = do
+    input <- readFile "../input"
+    let pairs = lines input
+    let ranges = map parseRanges pairs
+
+    let fullyOverlapsSum = foldr ((+) . fromEnum . uncurry fullyOverlaps) 0 ranges
+    putStrLn $ "First part: " ++ show fullyOverlapsSum
+
+    let overlapsSum = foldr ((+) . fromEnum . uncurry overlaps) 0 ranges
+    putStrLn $ "Second part: " ++ show overlapsSum
